a0045c8bd40cbce28684c49ceca2144294899572
Jordan Weichel <weichel@umich.edu>
2026-03-13 15:45:03 -0400

    Initial Next.js site-test

---
+++ src/app/page.tsx
@@ -0,0 +1,12 @@
+import styles from "./page.module.css";
+
+export default function Home() {
+  return (
+    <div className={styles.page}>
+      <main className={styles.main}>
+        <h1>bootytest</h1>
+        <p>site-test</p>
+      </main>
+    </div>
+  );
+}
